7fbfa6427e66ac52e064063ba2c9542958e426ad
Philippe Timothe <PhilippeTimothe@gmail.com>
2023-07-07 13:09:18 -0400

    Revised dataframe details

--- PredictUI.py
+++ PredictUI.py
@@ -4,61 +4,55 @@ import streamlit as st
 
 
 def load_model():
-    with open('Resources/salaries_pred_model.pkl', 'rb') as file:
+    with open("Resources/salaries_pred_model.pkl", "rb") as file:
         model_data = pickle.load(file)
     return model_data
+
 
 model_data = load_model()
 
 regressor = model_data["model"]
 experience_level = model_data["experience_level"]
-employment_type = model_data["employment_type"]
 job_title = model_data["job_title"]
 company_location = model_data["company_location"]
 company_size = model_data["company_size"]
+
 
 def show_prediction_page():
     st.title("Data Science Salary Predictions")
 
     st.write("""### Input selection needed to predict salary""")
 
-    experience_levels = (
-        "EX",
-        "EN",
-        "MI",
-        "SE"
-    )    
+    experience_levels = ("Executive", "Senior Executive", "Mid Level", "Entry Level")
 
-    employment_types = (
-        "FT",
-    )
+    experience_l_map = {
+        "Executive": "EX",
+        "Senior Executive": "SE",
+        "Mid Level": "MI",
+        "Entry Level": "EN",
+    }
 
     job_titles = (
         "Data Engineer",
         "Data Scientist",
         "Data Analyst",
-        "Machine Learning Engineer"
+        "Machine Learning Engineer",
     )
 
-    remote_ratios = (
-        100,
-        0
-    )
+    remote_ratios = (100, 0)
 
-    company_locations = (
-        "US",
-        "GB"
-    )
+    company_locations = ("United States", "Great Britain")
 
-    company_sizes = (
-        "S",
-        "M",
-        "L"
-    )
+    company_locations_map = {
+        "United States": "US",
+        "Great Britain": "GB",
+    }
 
-    work_year = st.slider("Work Year", 2020, 2023, 2023)
+    company_sizes = ("Large", "Medium", "Small")
+
+    company_sizes_map = {"Large": "L", "Medium": "M", "Small": "S"}
+
     experience_l = st.selectbox("Experience Level", experience_levels)
-    employment_t = st.selectbox("Employment Type",employment_types)
     job_t = st.selectbox("Job Title", job_titles)
     remote = st.selectbox("Remote Percentage", remote_ratios)
     company_l = st.selectbox("Company Location", company_locations)
@@ -66,13 +60,22 @@ def show_prediction_page():
 
     calculate = st.button("Caluculate Salary Prediction")
     if calculate == True:
-        X = np.array([[work_year, experience_l, employment_t, job_t, remote, company_l, company_s]])
-        X[:, 1] = experience_level.transform(X[:, 1])
-        X[:, 2] = employment_type.transform(X[:, 2])
-        X[:, 3] = job_title.transform(X[:, 3])
-        X[:, 5] = company_location.transform(X[:, 5])
-        X[:, 6] = company_size.transform(X[:, 6])
-        X = X.astype('int64')
+        X = np.array(
+            [
+                [
+                    experience_l_map[experience_l],
+                    job_t,
+                    remote,
+                    company_locations_map[company_l],
+                    company_sizes_map[company_s],
+                ]
+            ]
+        )
+        X[:, 0] = experience_level.transform(X[:, 0])
+        X[:, 1] = job_title.transform(X[:, 1])
+        X[:, 3] = company_location.transform(X[:, 3])
+        X[:, 4] = company_size.transform(X[:, 4])
+        X = X.astype("int64")
 
         salary = regressor.predict(X)
         st.subheader(f"Predicted salary is ${salary[0]:.2f}")
--- ResultsUI.py
+++ ResultsUI.py
@@ -53,32 +53,5 @@ def load_data():
 
 salary_df = load_data()
 
-
-def show_exploration_page():
-    st.title("Explore Data Science Salaries Data")
-    st.write("""### Kaggle Data Science Salaries 2023""")
-
-    # Plot Job Title Data
-    job_title_data = salary_df["job_title"].value_counts()
-    colors = plt.get_cmap("Blues")(np.linspace(0.2, 0.7, len(job_title_data)))
-
-    fig, ax = plt.subplots()
-    wedges, texts, autotexts = ax.pie(
-        job_title_data,
-        colors=colors,
-        radius=3,
-        center=(4, 4),
-        autopct="%1.1f%%",
-        wedgeprops={"linewidth": 1, "edgecolor": "white"},
-    )
-    ax.axis("equal")
-    ax.legend(
-        wedges,
-        job_title_data.index,
-        title="Ingredients",
-        loc="center left",
-        bbox_to_anchor=(1, 0, 0.5, 1),
-    )
-
-    st.write("""#### Data Science Job Titles""")
-    st.pyplot(fig)
+def show_results_data(experience_level, job_title, remote_status, company_location, company_size):
+    
